d61cb8fb96b946f0f1bdf019d5aab71a2a8bef3a
Ayush Rajpal <ayushrajpal2021@gmail.com>
2023-10-10 22:44:01 +0530

    todays changes

--- learning/conditions.py
+++ learning/conditions.py
@@ -1,7 +1,48 @@
-a = int(input("Enter your age "))
-print("your age is",a)
+# a = int(input("Enter your age "))
+# print("your age is",a)
 
-if(a>=18):
-    print("You are an adult so you can drive a car for sure!")
+# if(a>=18):
+#     print("You are an adult so you can drive a car for sure!")
+# else:
+#     print("Sorry dear you drink milk and sleep")
+
+# here we use "elif" wheras we use else if in c++
+
+num = 50
+
+if(num>100):
+    print("num>40")
+elif(num >35):
+    print("num >45")
+elif(num>20):
+    print("num >50")
 else:
-    print("Sorry dear you drink milk and sleep")
+    print("num is rest everything")
+
+print("the if-else statment is exicuted")
+
+#good mornig sir program
+
+import time
+timestamp =time.strftime('%H') 
+print(timestamp)
+if(int(timestamp) < 12):
+    print("Good Morning Sir, have a good day ahead")
+else:
+    print("Good Evening Sir!!")
+
+# python also have switch case type thing named as match
+"""
+syntax as follows: -->
+
+match x:  # x is a variable
+    case 0:
+        print("something")
+    case _:
+        print("default case")
+
+--> here break statment is not needed; <--
+--> can use a range of value in default by using conditions
+ex - case _ if x!=90:
+-->and we can use many default statment because of this
+"""
